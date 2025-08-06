70332851234db0565a371879c7aab9c75a3a6c5b
sarchiii <kevinsvrchi@gmail.com>
2025-08-06 18:21:29 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import pattern from "./Daisy_Pattern-DaisyField_Enamel.svg";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        {/* <img src={pattern} className='Bloom-pattern' alt="logo"/> */}
+        <div className="Footer">
+          <span>PRESENTED</span>
+          <span>by Daisy</span>
+        </div>
       </header>
     </div>
   );
